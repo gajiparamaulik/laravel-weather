3b7c8f43d8b2ddc6713bbdd12253956be89d8c75
Maulik Gajipara <gajiparamaulik@gmail.com>
2023-05-29 01:17:25 +0530

    add link ref

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,18 @@
     <script src="{{ asset('js/home.js') }}" defer></script>
 </head>
     <article class="widget">
-        <div class="weatherIcon"></div>
-        <div class="weatherData">
-            <h1 class="temperature temp-font">{{ $location['main']['temp'] - 273.15 }} <span>&#8451;</span></h1>
-            <h2 class="description">{{ $location['weather'][0]['description'] }}</h2>
-            <h3 class="city">{{ $location['name']}} , {{ $location['sys']['country'] }}.</h3>
-        </div>
-        
-        <div class="date">
-            <h4 class="month" id="month"></h4>
-            <h5 class="day" id="day"></h5>
-        </div>
+        <a href="/home">
+            <div class="weatherIcon"></div>
+            <div class="weatherData">
+                <h1 class="temperature temp-font">{{ $location['main']['temp'] - 273.15 }} <span>&#8451;</span></h1>
+                <h2 class="description">{{ $location['weather'][0]['description'] }}</h2>
+                <h3 class="city">{{ $location['name']}} , {{ $location['sys']['country'] }}.</h3>
+            </div>
+            
+            <div class="date">
+                <h4 class="month" id="month"></h4>
+                <h5 class="day" id="day"></h5>
+            </div>
+        </a>
     </article>
 @endsection
